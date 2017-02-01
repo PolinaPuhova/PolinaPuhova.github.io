494633044c662f0b2d341b190a24b7e5576fed34
Pcheluntik <polli-puh@mail.ru>
2017-02-02 01:10:39 +0300

    Добавлен слайдер

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,31 @@
-function() {
 
-};
+  var sliderList = document.getElementById('slider-ul');
+  var leftArrow = document.querySelector('.slider-left-arrow');
+  var rightArrow = document.querySelector('.slider-right-arrow');
+  var slide = document.querySelector('.slider-item');
+  var widthSlide = slide.clientWidth;
+  var position = 0;
+
+  console.log(widthSlide);
+
+  leftArrow.addEventListener("click", function(event) {
+    event.preventDefault();
+    margin = window.getComputedStyle(sliderList).marginLeft;
+    position = Number(margin.substring(0,margin.length-2));
+    console.log(position);
+    position =  position + widthSlide;
+    sliderList.style.marginLeft = position + 'px';
+    console.log(position + 'px');
+    position = 0;
+  });
+
+  rightArrow.addEventListener("click", function(event) {
+    event.preventDefault();
+    margin = window.getComputedStyle(sliderList).marginLeft;
+    position = Number(margin.substring(0,margin.length-2));
+    console.log(position);
+    position =  position - widthSlide;
+    sliderList.style.marginLeft = position + 'px';
+    console.log(position + 'px');
+    position = 0;
+  });
