7c3a781514d85cbf4614a8573f4b720f672f23aa
Pcheluntik <polli-puh@mail.ru>
2017-02-08 01:11:21 +0300

    Добавлен плавный скролл и normaize

--- js/slider.js
+++ js/slider.js
@@ -1,27 +1,33 @@
+'use strict';
+
 var sliderList = document.getElementById('slider-ul');
 var leftArrow = document.querySelector('.slider-left-arrow');
 var rightArrow = document.querySelector('.slider-right-arrow');
-var slide = document.querySelector('.slider-item');
-var widthSlide = 0;
-var position = 0;
+var slide = null;
 
 //событие на нажатие стрелки влево
 leftArrow.addEventListener("click", function(event) {
   event.preventDefault();
+  //ищем слайды
+  slide = document.querySelectorAll('.slider-item');
 
   //находим элементы первого и последнего слайда
-  var firstSlide = sliderList.childNodes[1];
-  var delSlide = sliderList.childNodes[3];
+  var firstSlide = slide[0];
+  var i = slide.length - 1;
+  var delSlide = slide[i];
 
   //вставляем вперед последний слайд, удаляя при этом его
   sliderList.insertBefore(delSlide, firstSlide);
 });
 
+//событие на нажатие стрелки вправо
 rightArrow.addEventListener("click", function(event, copySlide) {
   event.preventDefault();
+  //ищем слайды
+  slide = document.querySelectorAll('.slider-item');
 
   //находим первый элемент
-  var delSlide = sliderList.childNodes[1];
+  var delSlide = slide[0];
 
   //вставляем первый слайд в конец
   sliderList.appendChild(delSlide);
